1c39de850d4a08f9fd9f29baa126b50974fb0a36
Baurice <bauricenafack@gmail.com>
2023-01-27 17:52:46 +0100

    update

--- generic_algorithm.py
+++ generic_algorithm.py
@@ -5,20 +5,22 @@ import math
 import warnings
 warnings.filterwarnings("ignore")
 
-
 df = pd.read_excel('products.xlsx')
 obj= np.array(df['Product'])
 space = np.array(df['Space'])
 
 
 function_inputs = space # Function space.
-desired_output = 3 # Function output.
-sol_per_population = int(math.pow(2,11))
+desired_output = 4.4 # volume capacity we want to reach.
+sol_per_population = int(math.pow(2,11)) # 2^11 number of sample in the initial population
+
 
 def fitness_func(solution, solution_idx):
-    # The fitness function calulates the sum of products between each input and its corresponding space.
+    """This function calculates the total fitness value by summing the product of 
+    each input and its corresponding space.
+    """
     output = np.sum(solution*function_inputs)
-    if (output>desired_output): 
+    if (output>desired_output): # discard a sample if the sum is more than the space available
         fitness=0.0
     else:
         # The value 0.000001 is used to avoid the Inf value when the denominator numpy.abs(output - desired_output) is 0.0.
@@ -26,6 +28,10 @@ def fitness_func(solution, solution_idx):
     return fitness
 
 def print_selected_obj(ga_instance,obj,space):
+
+    """This function generates all van loading combinations 
+    considering capacity, weight and size. It returns a list of possible combinations.
+    """
     solution,_,_ = ga_instance.best_solution()
     solution = np.array(solution).astype(float)
     print('\n')
@@ -34,25 +40,29 @@ def print_selected_obj(ga_instance,obj,space):
     print(f'object selected {obj[np.where(solution == 1)]}')
     print('\n')
 
+
+
 def save_model(ga_instance):
     filename = 'genetic'
     ga_instance.save(filename=filename)
+
 
 def plot_fitnes(ga_instance):
     ga_instance.plot_fitness(title="PyGAD with Adaptive Mutation", linewidth=5)
 
 
 def show_best_genes(ga_instance):
+    """The function will identify the genes (inputs) with 
+    the highest fitness value and display them as the best options.
+    """
     solution, solution_fitness, solution_idx = ga_instance.best_solution()
+
     print('\n')
     print("Parameters of the best solution : {solution}".format(solution=solution))
     print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
     print('\n')
 
-    #Let's save the model.  
-
-    
 
 # Creating an instance of the GA class inside the ga module. Some parameters are initialized within the constructor.
 ga_instance = pygad.GA(num_generations=100,
@@ -78,6 +88,8 @@ if __name__=='__main__':
     print_selected_obj(ga_instance,obj,space)
 
     show_best_genes(ga_instance)
+    #Let's save the model.  
+
     save_model(ga_instance)
     plot_fitnes(ga_instance)
     
